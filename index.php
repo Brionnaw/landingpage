fbefdc65e7bf638997fa40062f3749c88f333341
Brionna White <Brionnanwhite@gmail.com>
2016-12-19 11:49:08 -0800

    add

--- index.php
+++ index.php
@@ -1,38 +1,46 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <meta charset="utf-8">
     <title>Mnkny.com</title>
-    <link rel="shortcut icon" href="images/icon1.png" type="image/x-icon" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta class="visible-xs" name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no, width=device-width">
+    <base href="/" />
+    <link href="style.css" rel="stylesheet"/>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.1/animate.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-    <link href="style.php" rel="stylesheet"/>
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url')?>" type="text/css">
   </head>
   <body>
-    <div class="col-xs-12"></div>
+    <!-- desktop-->
+      <div class="col-xs-12 col-sm-12 col-md-12"></div>
 
     <!-- subscription form-->
-    <div class="col-xs-12"> <p class="text">  Organic | Sustainable | Leisurewear </p></div>
-    <div class="col-xs-12" style="padding:20px 40px 0px 80px">
-
-        <p style="font-size:45px"><strong>Coming Soon 2017</strong></p>
-    <label>Form action</label>
-    <label>
-        <input name="_mc4wp_action" type="radio" value="subscribe" checked> <span>Subscribe</span>
-    </label>
-    <label>
-        <input name="_mc4wp_action" type="radio" value="unsubscribe"><span>Unsubscribe</span>
-    </label>
-  </p>
-  <!-- Begin MailChimp Signup Form -->
-  <link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
-  <style type="text/css">
-	 #mc_embed_signup{background:transparent; clear:left; font:14px Helvetica,Arial,sans-serif; width:100%;}
-  	/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+    <div class="row">
+          <div class="col-xs-12 col-sm-12 col-md-12"> <p class="text">  Organic | Sustainable | Leisurewear </p></div>
+    <div class="col-xs-12 col-sm-12 col-md-12">
+    </div>
+  </div>
+    <div class="row form">
+      <p class="subtext"><strong>Coming Soon 2017</strong></p>
+      <div class="col-xs-12 col-sm-12 col-md-12">
+        <label>Form action</label>
+        <label>
+          <input name="_mc4wp_action" type="radio" value="subscribe" checked> <span>Subscribe</span>
+        </label>
+        <label>
+          <input name="_mc4wp_action" type="radio" value="unsubscribe"><span>Unsubscribe</span>
+        </label>
+      </p>
+      <!-- Begin MailChimp Signup Form -->
+      <link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
+      <style type="text/css">
+	     #mc_embed_signup{background:transparent; clear:left; font:14px Helvetica,Arial,sans-serif; width:100%;}
+  	    /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
 	     We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
        </style>
+
        <div id="mc_embed_signup">
          <div id="mc_embed_signup_scroll">
 	          <label for="mce-EMAIL">Sign up below and we'll notify you of our launch</label>
@@ -44,11 +52,12 @@
          </div>
          <!--End mc_embed_signup-->
        </div>
+     </div>
        <!--FOOTER-->
        <footer>
          <div class="row">
-           <div class="col-xs-12" style="padding:250px 40px 0px 180px">
-             <p class="text-center">
+           <div class="col-xs-12 col-sm-12 col-md-12">
+             <p class="text-center footer">
                &copy; 2016 MNKNJY | <a href="https://brionnaw.github.io/hireme/" style="color:#000000">Site By Brionna</a></strong>
              </p>
            </div>
